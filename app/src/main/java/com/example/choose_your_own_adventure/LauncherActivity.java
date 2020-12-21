dffc1ed6eaa3ce021a4007cdd24e1cf67a294c6e
qbielka <qjbielka@hotmail.com>
2020-12-21 10:45:51 -0800

    Init Commit

---
+++ app/src/main/java/com/example/choose_your_own_adventure/LauncherActivity.java
@@ -0,0 +1,14 @@
+package com.example.choose_your_own_adventure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
