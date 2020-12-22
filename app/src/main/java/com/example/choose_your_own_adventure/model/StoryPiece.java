df59cd709d03d646772cd1c4f3acb4e79f31c962
quince <qbielka@sfu.ca>
2020-12-22 13:34:19 -0800

    added comments

--- app/src/main/java/com/example/choose_your_own_adventure/model/StoryPiece.java
+++ app/src/main/java/com/example/choose_your_own_adventure/model/StoryPiece.java
@@ -2,11 +2,19 @@ package com.example.choose_your_own_adventure.model;
 
 import java.util.ArrayList;
 
+/**
+ * StoryPiece is the textual content of a single frame of the story.
+ *  It holds the list of options as well as the explanation of what just happened as well as what is happening now
+ */
 public class StoryPiece {
 
     private String story;
     private ArrayList<String> buttons;
 
+    /**
+     * This is the constructor for StoryPiece it required
+     * @param story is the text that will be shown in the UI that is not a button
+     */
     public StoryPiece(String story){
         this.story = story;
     }
@@ -22,6 +30,7 @@ public class StoryPiece {
         buttons.add(buttonText);
         return buttons.size();
     }
+
 
     public String getStory() {
         return story;
