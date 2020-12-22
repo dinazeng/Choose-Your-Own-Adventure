f24d65a77828379c58b69f679fe8b0c914a42bbf
quince <qbielka@sfu.ca>
2020-12-22 13:08:19 -0800

    added a default storyModel

--- app/src/main/java/com/example/choose_your_own_adventure/model/StoryModel.java
+++ app/src/main/java/com/example/choose_your_own_adventure/model/StoryModel.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 //TODO this class
 public class StoryModel {
 
+    private int storyBoard = 0;
     public void button1Pushed(){}
 
     public void button2Pushed(){}
@@ -15,8 +16,11 @@ public class StoryModel {
 
     public StoryPiece getNextStory(){
 
-        StoryPiece storyPiece = new StoryPiece("Default Button");
-
+        StoryPiece storyPiece = new StoryPiece("Default Story Piece");
+        storyPiece.addButton("Default Button "+(1+storyBoard));
+        storyPiece.addButton("Default Button "+(2+storyBoard));
+        storyPiece.addButton("Default Button "+(3+storyBoard));
+        storyBoard += 3;
         return storyPiece;
     }
 
