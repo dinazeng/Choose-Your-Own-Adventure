a1a92a75951c8225b1e05ca0d59ed560d7dbf274
quince <qbielka@sfu.ca>
2020-12-22 11:28:34 -0800

    Adds a facade class so that we can get to work on the UI pieces that call this before we need to make this

---
+++ app/src/main/java/com/example/choose_your_own_adventure/model/StoryModel.java
@@ -0,0 +1,23 @@
+package com.example.choose_your_own_adventure.model;
+
+import java.util.ArrayList;
+
+//TODO this class
+public class StoryModel {
+
+    public void button1Pushed(){}
+
+    public void button2Pushed(){}
+
+    public void button3Pushed(){}
+
+    public void button4Pushed(){}
+
+    public StoryPiece getNextStory(){
+
+        StoryPiece storyPiece = new StoryPiece("Default Button");
+
+        return storyPiece;
+    }
+
+}
