7998aafc0921b1c6aaa733efba92929f9b625b92
quince <qbielka@sfu.ca>
2020-12-23 12:39:38 -0800

    Move UI segment to its own package

--- app/src/main/java/com/qbielka/dinazeng/choose_your_own_adventure/UI/LauncherActivity.java
+++ app/src/main/java/com/qbielka/dinazeng/choose_your_own_adventure/UI/LauncherActivity.java
@@ -1,16 +1,18 @@
-package com.qbielka.dinazeng.choose_your_own_adventure;
+package com.qbielka.dinazeng.choose_your_own_adventure.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.qbielka.dinazeng.choose_your_own_adventure.R;
+
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
         Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
         startActivity(intent);
     }
