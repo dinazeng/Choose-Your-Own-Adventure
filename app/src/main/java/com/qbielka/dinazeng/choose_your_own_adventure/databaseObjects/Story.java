8fe2a900aa4375d87e881dce214f3614d17f1d2c
quince <qbielka@sfu.ca>
2020-12-29 17:01:51 -0800

    CSV Parser set up

--- app/src/main/java/com/qbielka/dinazeng/choose_your_own_adventure/databaseObjects/Story.java
+++ app/src/main/java/com/qbielka/dinazeng/choose_your_own_adventure/databaseObjects/Story.java
@@ -23,8 +23,36 @@ public class Story {
     private String button4Text;
     private int button4NextState;
 
-    public Story(String story){
+    public Story(String CSVLine){
         //todo fix this so it sets all values of  class.
+
+        ArrayList<String> columns = breakCSVLineIntoColumns(CSVLine);
+
+        id = Integer.parseInt(columns.get(0));
+        story = columns.get(1);
+        numButtons = Integer.parseInt(columns.get(2));
+
+        int b1Position = 3;
+        int b2Position = 4;
+        int b3Position = 5;
+        int b4Position = 6;
+
+        if(numButtons > 0){
+            button1Text = columns.get(b1Position);
+            button1NextState = Integer.parseInt(columns.get(b1Position+numButtons));
+        }
+        if(numButtons > 1){
+            button1Text = columns.get(b2Position);
+            button1NextState = Integer.parseInt(columns.get(b2Position+numButtons));
+        }
+        if(numButtons > 2){
+            button1Text = columns.get(b3Position);
+            button1NextState = Integer.parseInt(columns.get(b3Position+numButtons));
+        }
+        if(numButtons > 3){
+            button1Text = columns.get(b4Position);
+            button1NextState = Integer.parseInt(columns.get(b4Position+numButtons));
+        }
 
         //todo talk to Dina about code flexibility
 //        id = Integer.valueOf(story.charAt(0));
@@ -46,6 +74,51 @@ public class Story {
 //        button1NextState = Integer.valueOf(story.charAt(0));
 //        button2NextState = Integer.valueOf(story.charAt(2));
 //        button3NextState = Integer.valueOf(story.charAt(4));
+    }
+
+    private static ArrayList<String> breakCSVLineIntoColumns(String CSVLine){
+        ArrayList<String> arr = new ArrayList<>();
+
+        for(int index = 0; index < CSVLine.length(); index++){
+            // Escape Characters will be handled
+            if(CSVLine.charAt(index) == '\\'){
+                //todo figure out how I handle escape chars
+                // for now skip the next character like it doesn't exist
+                index++;
+            }
+            //Handling for a Quote Mark, note that this is not an escaped quote mark
+            else if(CSVLine.charAt(index) == '\"'){
+                // first iterate to the next character
+                index++;
+                // then check for the next unescaped Quote mark
+                for (;index < CSVLine.length(); index++){
+                    // Ensure no escaped Quote marks break out of the enclosed quotes
+                    if(CSVLine.charAt(index) == '\\'){
+                        index++;
+                        continue;
+                    }
+                    //A Second Quote mark has been found
+                    if(CSVLine.charAt(index) == '\"'){
+                        break;
+                    }
+                }
+            }
+            // Handling a comma
+            else if(CSVLine.charAt(index) == ','){
+                // Cell starts with the first character and ends the character before the comma
+                String cell = CSVLine.substring(0,index);
+                CSVLine = CSVLine.substring(index + 1);
+                // the negative 1 will roll over into 0 for the next iteration of the loop
+                index = -1;
+                arr.add(cell);
+            }
+
+        }
+        // Handling hitting the end of input
+        arr.add(CSVLine);
+
+        // Return Statement
+        return arr;
     }
 
     public int getId() {
