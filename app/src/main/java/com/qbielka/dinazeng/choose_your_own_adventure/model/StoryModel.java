63d2d0597a4b4a852bf160cbeab5c0ea68b049ca
quince <qbielka@sfu.ca>
2021-01-04 22:29:01 -0800

    Fix small error that exists in the Model
    StoryPiece needs multiple button effects
    StoryModel needs to refer to the correct button

--- app/src/main/java/com/qbielka/dinazeng/choose_your_own_adventure/model/StoryModel.java
+++ app/src/main/java/com/qbielka/dinazeng/choose_your_own_adventure/model/StoryModel.java
@@ -16,7 +16,13 @@ public class StoryModel {
         thisStory = db.getStory(key);
     }
 
+    /**
+     * This takes in the number of the button that was pushed and triggers its effect
+     * @param num
+     */
     public void buttonNPushed(int num){
+        // Buttons start numbered at 1 however the array starts them at 0
+        num = num -1;
         int nextKey =  thisStory.getButtonArr().get(num).getButtonKey();
         thisStory = db.getStory(nextKey);
         getNextStory();
